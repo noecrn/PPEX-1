e05b7c2b8d508047e872ae576c7e1760d1192f5a
noe.cornu <noe.cornu1@gmail.com>
2025-10-31 14:52:09 +0100

    [UPDATE] Code coverage

---
+++ minimake/src/dlist/dlist.c
@@ -0,0 +1,58 @@
+#include <assert.h>
+#include <err.h>
+#include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dlist.h"
+
+struct dlist *dlist_init(void)
+{
+    struct dlist *new_list = malloc(sizeof(struct dlist));
+    if (!new_list)
+        return NULL;
+
+    new_list->size = 0;
+    new_list->head = NULL;
+    new_list->tail = NULL;
+    return new_list;
+}
+
+int dlist_push_back(struct dlist *list, void *element)
+{
+    struct dlist_item *new_item = malloc(sizeof(struct dlist_item));
+    if (!new_item)
+        return 0;
+
+    new_item->data = element;
+    new_item->prev = list->tail;
+    new_item->next = NULL;
+
+    if (list->size != 0)
+        list->tail->next = new_item;
+    list->tail = new_item;
+    if (list->size == 0)
+        list->head = new_item;
+    list->size++;
+
+    return 1;
+}
+
+void dlist_clear(struct dlist *list)
+{
+    if (!list || list->size == 0)
+        return;
+
+    struct dlist_item *cur = list->head;
+    while (cur != NULL)
+    {
+        cur = list->head->next;
+        free(list->head);
+        list->head = cur;
+    }
+
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+}
--- minimake/src/dlist/dlist.h
+++ minimake/src/dlist/dlist.h
@@ -17,23 +17,8 @@ struct dlist
     struct dlist_item *tail;
 };
 
-// Threshold 1
 struct dlist *dlist_init(void);
-int dlist_push_front(struct dlist *list, void *element);
-void dlist_print(const struct dlist *list);
 int dlist_push_back(struct dlist *list, void *element);
-size_t dlist_size(const struct dlist *list);
-
-// Threshold 2
-char *dlist_get(const struct dlist *list, size_t index);
-int dlist_insert_at(struct dlist *list, char *element, size_t index);
-int dlist_find(const struct dlist *list, char *element);
-char *dlist_remove_at(struct dlist *list, size_t index);
 void dlist_clear(struct dlist *list);
-
-// Threshold 3
-void dlist_reverse(struct dlist *list);
-struct dlist *dlist_split_at(struct dlist *list, size_t index);
-void dlist_concat(struct dlist *list1, struct dlist *list2);
 
 #endif /* ! DLIST_H */
